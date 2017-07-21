ec61dbd9593e26638dbaadfe84948311a89d3e3d
Phelipe Maia <phelipemaia@gmail.com>
2017-07-21 16:01:27 +0200

    Added user

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 const restify = require('restify');
+const Promise = require('bluebird');
+const mongoose = require('mongoose');
+
+const user = require('./src/model/user');
+
+mongoose.connect('mongodb://root:Nunc4Violara@ds027165.mlab.com:27165/gramofon');
+
+var db = mongoose.connection;
+db.on('error', console.log(console, 'connection error:'));
+db.once('open', function() {
+    console.log(console, 'connected to db')
+});
+
+var phelipe = new User({id: 123, name: 'Phelipe'});
+
+phelipe.save();
 
 function respond(req, res, next) {
     res.send('hello' + req.params.name);
---
+++ src/model/user.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({id: Number, name: String});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
